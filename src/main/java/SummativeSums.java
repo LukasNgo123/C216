8ecf1a1d669e78dc11f8f7fae3e724ffe76251ee
Lukas Ngo <ngolukas@gmail.com>
2022-04-21 18:21:25 +0100

    005: Summative Sums

---
+++ src/main/java/SummativeSums.java
@@ -0,0 +1,20 @@
+public class SummativeSums {
+    public static void main(String[] args) {
+        int[] array1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
+        int[] array2 = { 999, -60, -77, 14, 160, 301 };
+        int[] array3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
+                140, 150, 160, 170, 180, 190, 200, -99 };
+
+        System.out.println("#1 Array Sum: " + SumArray(array1));
+        System.out.println("#2 Array Sum: " + SumArray(array2));
+        System.out.println("#3 Array Sum: " + SumArray(array3));
+    }
+
+    private static int SumArray(int[] array){
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+            result += array[i];
+        }
+        return result;
+    }
+}
